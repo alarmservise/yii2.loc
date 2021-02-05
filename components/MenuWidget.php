d75bb5295660fd7401b9b30fc2e2623c4d93ee1d
Андрей Кузнецов <alarmservise@mail.ru>
2021-02-05 20:40:38 +0300

    4 урок часть 2 Х 03:10

---
+++ components/MenuWidget.php
@@ -0,0 +1,52 @@
+<?php
+
+
+namespace app\components;
+
+
+use app\models\Category;
+use yii\base\Widget;
+
+class MenuWidget extends Widget
+{
+    public $tpl;
+    public $ul_class;
+    public $data;
+    public $tree;
+    public $menuHtml;
+
+    public function init()
+    {
+        parent::init();
+        if($this->ul_class === null) {
+            $this->ul_class = 'menu';
+        }
+        if($this->tpl === null) {
+            $this->tpl = 'menu';
+        }
+        $this->tpl .= '.php';
+
+    }
+
+    public function run()
+    {
+        $this->data = Category::find()->select()->indexBy('id')->asArray()->all();
+        debug($this->data);
+
+        return $this->tpl;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
