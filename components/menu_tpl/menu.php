e45ab38363e603e087477c514bd08cbf78cdd8f9
Андрей Кузнецов <alarmservise@mail.ru>
2021-02-06 11:40:49 +0300

    4 урок часть 2 Х 21:46

--- components/menu_tpl/menu.php
+++ components/menu_tpl/menu.php
@@ -1,4 +1,4 @@
-<li>
+<li <? if(isset($category['children'])) echo 'class = "dropdown"' ?>>
     <a href="<?= \yii\helpers\Url::to(['category/view', 'id' => $category['id']]) ?>>">
         <?= $category['title']?>
     </a>
