b7805c781e856cb70089a5b375dfb94a533c6305
Андрей Кузнецов <alarmservise@mail.ru>
2021-02-06 11:34:34 +0300

    4 урок часть 2 Х 19:30

--- components/MenuWidget.php
+++ components/MenuWidget.php
@@ -1,8 +1,6 @@
 <?php
 
-
 namespace app\components;
-
 
 use app\models\Category;
 use yii\base\Widget;
@@ -31,9 +29,33 @@ class MenuWidget extends Widget
     public function run()
     {
         $this->data = Category::find()->select('id,parent_id,title')->indexBy('id')->asArray()->all();
-        debug($this->data);
+        $this->tree = $this->getTree();
+        $this->menuHtml = $this->getMenuHtml($this->tree);
+        return $this->menuHtml;
+    }
 
-        return $this->tpl;
+    protected function getTree()
+    {
+        $tree = [];
+        foreach ($this->data as $id => &$node) {
+            if (!$node['parent_id'])
+                $tree[$id] = &$node;
+            else
+                $this->data[$node['parent_id']] ['children'] [$node['id']] = &$node;
+        }
+        return $tree;
+    }
+    protected function getMenuHtml($tree) {
+        $str = '';
+        foreach ($tree as $category) {
+            $str .= $this->catToTemplate($category);
+        }
+        return $str;
+    }
+    protected  function catToTemplate($category) {
+        ob_start();
+        include  __DIR__ . '/menu_tpl/' . $this->tpl;
+        return ob_get_clean();
     }
 }
 
