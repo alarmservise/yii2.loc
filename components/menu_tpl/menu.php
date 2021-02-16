2bd60a5c470a207446e52d5fa25c41e045f380a7
Андрей Кузнецов <alarmservise@mail.ru>
2021-02-16 08:21:46 +0300

    4 урок часть 2 Х 24:00

--- components/menu_tpl/menu.php
+++ components/menu_tpl/menu.php
@@ -1,5 +1,15 @@
 <li <? if(isset($category['children'])) echo 'class = "dropdown"' ?>>
-    <a href="<?= \yii\helpers\Url::to(['category/view', 'id' => $category['id']]) ?>>">
+    <a href="<?= \yii\helpers\Url::to(['category/view', 'id' => $category['id']]) ?>>" <? if(isset($category['children'])) echo 'class = "dropdown-toggle" data-toggle="dropdown"' ?>>
         <?= $category['title']?>
     </a>
+    <? if(isset($category['children'])): ?>
+        <div class="dropdown-menu mega-dropdown-menu w3ls_vegetables_menu">
+            <div class="w3ls_vegetables">
+                <ul>
+                    <li><a href="drinks.html">Soft Drinks</a></li>
+                    <li><a href="drinks.html">Juices</a></li>
+                </ul>
+            </div>
+        </div>
+    <? endif; ?>
 </li>
