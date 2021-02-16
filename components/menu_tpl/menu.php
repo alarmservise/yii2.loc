c751c5db8c2f78a97bc784e1405b2f1a9f4a25c4
Андрей Кузнецов <alarmservise@mail.ru>
2021-02-16 09:09:55 +0300

    4 урок часть 2 Х 24:00 test1

--- components/menu_tpl/menu.php
+++ components/menu_tpl/menu.php
@@ -7,7 +7,7 @@
             <div class="w3ls_vegetables">
                 <ul>
                     <li><a href="drinks.html">Soft Drinks Test</a></li>
-                    <li><a href="drinks.html">Juices</a></li>
+                    <li><a href="drinks.html">Juice</a></li>
                 </ul>
             </div>
         </div>
